0cd653626c985d8a3c9674a956b57e934f85ba2f
IlmaxRehman <ilmarehman022@gmail.com>
2026-06-23 10:54:28 +0530

    improved hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,10 +2,6 @@ export default function Hero() {
   return (
     <div className="text-center mb-10">
 
-      <div className="inline-block bg-purple-100 text-purple-700 px-4 py-2 rounded-full mb-5">
-        Powered by Gemini AI
-      </div>
-
       <h1 className="text-6xl font-bold text-slate-800">
         BulletCraft
         <span className="text-pink-500"> AI</span>
